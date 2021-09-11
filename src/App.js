98f054e934044ff1212480423c586af88822f90a
git-0r <rohitdec97@gmail.com>
2021-09-11 14:26:51 +0530

    complete basic todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [allNotes, setAllNotes] = useState([])
+  const [modal, setModal] = useState(false)
+  const [editNote, setEditNote] = useState({})
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/notes')
+      .then(res => setAllNotes(res.data))
+  }, [])
+
+  const createNote = (e) => {
+    e.preventDefault()
+
+    const content = e.target[0].value
+
+    if (content === "") {
+      window.alert("content is missing !")
+    } else {
+      axios.post('http://localhost:3001/notes', { content: content })
+        .then(res => setAllNotes(allNotes.concat(res.data)))
+      e.target[0].value = ""
+    }
+  }
+
+  const remove = (note) => {
+    axios.delete(`http://localhost:3001/notes/${note.id}`)
+
+    const notes = allNotes.filter(n => n.id !== note.id)
+    setAllNotes(notes)
+  }
+
+  const ShowNotes = ({ allNotes }) => {
+    return (
+      allNotes.map(note => {
+        return (
+          <div className="note-container" key={note.id}>
+            <pre><li>{note.content}</li></pre>
+            <div className="note-actions">
+              <button className="todo-icon" onClick={() => remove(note)}>
+                <ion-icon name="trash-outline"></ion-icon>
+              </button>
+              <button className="todo-icon" onClick={() => {
+                setModal(true);
+                setEditNote(note)
+              }}>
+                <ion-icon name="create-outline"></ion-icon>
+              </button>
+            </div>
+          </div>
+        )
+      })
+    )
+  }
+
+  const updateNote = (e) => {
+    e.preventDefault()
+
+    const content = e.target[0].value
+    const id = editNote.id
+    axios.put(`http://localhost:3001/notes/${id}`, { "content": content })
+
+    const updatedNotes = allNotes.map(note => {
+      if (note.id === id) {
+        return { "id": id, "content": content }
+      } else {
+        return note
+      }
+    })
+
+    setAllNotes(updatedNotes)
+    setModal(false)
+  }
+
+  const Modal = () => {
+    if (modal) {
+      return (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setModal(false)}>&times;</span>
+            <h2>Edit</h2>
+            <form onSubmit={e => updateNote(e)}>
+              <textarea placeholder="feed the cat..." defaultValue={editNote.content}></textarea>
+              <button className="btn-updateNote"><ion-icon name="add-outline"></ion-icon></button>
+            </form>
+          </div>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+  window.onclick = (e) => {
+    if (e.target.className === "modal") {
+      setModal(false)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TO-DO</h1>
+      <Modal />
+      <form onSubmit={e => createNote(e)}>
+        <textarea placeholder="feed the cat..."></textarea>
+        <button className="btn-addNote"><ion-icon name="add-outline"></ion-icon></button>
+      </form>
+      <ul>
+        <ShowNotes allNotes={allNotes} />
+      </ul>
+      <footer>
+        <p>Made with ❤️ by Rohit <a href="https://github.com/git-0r" target="_blank" rel="noreferrer"><ion-icon name="logo-github"></ion-icon></a></p>
+      </footer>
     </div>
   );
 }
